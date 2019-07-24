c66144e314193ad95e70bbfd9bb58eac3ca4c7a8
Taryn Neal <tarynneal10@gmail.com>
2019-07-24 14:23:54 -0700

    Changed to MVC pattern. Added some controllers.

---
+++ Leader/Controller/CompetitiveEventsVC.swift
@@ -0,0 +1,14 @@
+//
+//  CompetitiveEventsVC.swift
+//  Leader
+//
+//  Created by Taryn Neal on 7/24/19.
+//  Copyright © 2019 Taryn Neal. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CompetitiveEventsVC : UITableViewController {
+    
+}
---
+++ Leader/Controller/CurrentEventsVC.swift
@@ -0,0 +1,14 @@
+//
+//  CurrentEventsVC.swift
+//  Leader
+//
+//  Created by Taryn Neal on 7/24/19.
+//  Copyright © 2019 Taryn Neal. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CurrentEventsVC : UITableViewController {
+    
+}
