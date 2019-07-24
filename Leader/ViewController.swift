78e8a322207cb21e6cc3f9b602bcf5cdfbb88f9a
Taryn <tarynneal10@gmail.cpm>
2019-07-23 19:01:25 -0700

    Initial Commit

---
+++ Leader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Leader
+//
+//  Created by Taryn Neal on 7/23/19.
+//  Copyright © 2019 Taryn Neal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
