12733fa8ea829a99ed45415aeafe95869ba46f3c
Taryn <tarynneal10@gmail.cpm>
2019-07-23 21:17:26 -0700

    Minor setup.

---
+++ Leader/SettingsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SettingsViewController.swift
+//  Leader
+//
+//  Created by Taryn Neal on 7/23/19.
+//  Copyright © 2019 Taryn Neal. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SettingsVC : UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
