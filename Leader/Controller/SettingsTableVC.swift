c4f6e63f65a53c68bb417ba9812a604b34d39ba4
Taryn <tarynneal10@gmail.cpm>
2019-07-26 18:38:23 -0700

    Put in items for tab bar. Adding profile editor.

--- Leader/Controller/SettingsTableVC.swift
+++ Leader/Controller/SettingsTableVC.swift
@@ -11,8 +11,13 @@ import UIKit
 
 class SettingsVC : UITableViewController {
     
+    @IBOutlet weak var positionLabel: UILabel!
+    @IBOutlet weak var chapterLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var profilePicture: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        profilePicture.image = UIImage(named: "Logo2")
         // Do any additional setup after loading the view.
     }
     
